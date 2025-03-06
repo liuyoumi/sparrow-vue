b3e7bf41125b364c42ea7687a2a4da1d7390e1f3
liuyoumi <liuyoumi@hotmail.com>
2025-03-06 14:58:29 +0800

    chore: add UnoCSS plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import {resolve} from "path";
+import UnoCSS from "unocss/vite";
 import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    UnoCSS(),
+  ],
   resolve: {
     alias: [
       {
