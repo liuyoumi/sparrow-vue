76ff618131a7a13bddf124e94d416ac89a32c2fc
liuyoumi <liuyoumi@hotmail.com>
2025-03-06 15:26:47 +0800

    chore: load components configuration on demand

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
 import {resolve} from "path";
-import UnoCSS from "unocss/vite";
 import {defineConfig} from "vite";
+
 import vue from "@vitejs/plugin-vue";
+import UnoCSS from "unocss/vite";
+import Components from "unplugin-vue-components/vite";
+import {AntDesignVueResolver} from "unplugin-vue-components/resolvers";
 
 export default defineConfig({
   plugins: [
     vue(),
     UnoCSS(),
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false,
+        }),
+      ],
+    }),
   ],
   resolve: {
     alias: [
