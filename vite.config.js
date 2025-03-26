6eced67b8ae7d5f9c1901b61f9d31d3f453cdb2c
liuyoumi <liuyoumi@hotmail.com>
2025-03-26 14:52:53 +0800

    feat: add auto import support

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,25 @@
 import {resolve} from "path";
 import {defineConfig} from "vite";
 
-import vue from "@vitejs/plugin-vue";
+import Vue from "@vitejs/plugin-vue";
+import VueJsx from "@vitejs/plugin-vue-jsx";
 import UnoCSS from "unocss/vite";
 import {viteMockServe} from "vite-plugin-mock";
+import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import {AntDesignVueResolver} from "unplugin-vue-components/resolvers";
 
 export default defineConfig({
   plugins: [
-    vue(),
+    Vue(),
+    VueJsx(),
     UnoCSS(),
+    AutoImport({
+      imports: [
+        "vue",
+        "vue-router",
+      ],
+    }),
     Components({
       resolvers: [
         AntDesignVueResolver({
