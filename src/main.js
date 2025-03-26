67f830028d988fdb74929cdb1df4c5331b036bea
liuyoumi <liuyoumi@hotmail.com>
2025-03-26 20:26:03 +0800

    feat: use router,pinia etc.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
-// 引入 UnoCSS
+// UnoCSS
 import "@/plugins/unocss";
 
-// 引入全局样式
+// 全局样式
 import "@/assets/styles/index.css";
+
+// 动画样式
+import "animate.css";
+
+// 状态管理
+import store from "@/store/index.js";
+
+// 路由实例
+import router from "@/router/index.js";
 
 import {createApp} from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+// 路由权限
+import "./permission.js";
+
+const app = createApp(App);
+
+app.use(store);
+app.use(router);
+app.mount("#app");
