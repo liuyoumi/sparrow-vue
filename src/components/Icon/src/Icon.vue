2f37c95d6f0144d4885651159550db32a9346720
liuyoumi <liuyoumi@hotmail.com>
2025-03-28 16:59:16 +0800

    feat: integrate IconPark for improved icon support and Ant Design compatibility

--- src/components/Icon/src/Icon.vue
+++ src/components/Icon/src/Icon.vue
@@ -1,11 +1,15 @@
 <script setup>
+import {IconPark} from "@icon-park/vue-next/es/all.js";
 
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+});
 </script>
 
 <template>
-  $END$
+  <!-- 添加的两个className是为了适配 antd 原生的图标样式 -->
+  <IconPark :type="name" class="anticon anticon-pie-chart" style="vertical-align: -2px"/>
 </template>
-
-<style scoped>
-
-</style>
