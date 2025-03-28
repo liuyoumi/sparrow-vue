1f4c6b14ccde15f0aa5b0bdb807f813d5f596a86
liuyoumi <liuyoumi@hotmail.com>
2025-03-28 16:53:53 +0800

    refactor: optimize import statements by merging router modules and removing lodash-es

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,9 +1,6 @@
-import {defineStore} from "pinia";
-import _ from "lodash-es";
 import {AuthApi} from "@/api/auth/index.js";
-import {baseRoutes} from "@/router/index.js";
-import {generateRoutes} from "@/router/utils.js";
 import storage, {CACHE_KEY} from "@/utils/storage.js";
+import {baseRoutes, generateRoutes} from "@/router/routes.js";
 
 export const useUserStore = defineStore("user", {
   state: () => ({
@@ -25,7 +22,6 @@ export const useUserStore = defineStore("user", {
       this.name = data.name;
       this.roles = data.roles;
       this.perms = data.perms;
-      // 后端根据角色权限返回菜单列表，由前端处理菜单结构并转换成路由；
       this.routes = _.cloneDeep(baseRoutes).concat(generateRoutes(data.menus));
       this.activated = true;
     },
