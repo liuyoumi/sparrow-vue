dac6383dbef69a7de3263dff877db88b2c12dfe5
liuyoumi <liuyoumi@hotmail.com>
2025-04-01 14:54:43 +0800

    feat: dynamic switch router-view & use app theme

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <script setup>
+import Layout from "@/layout/Layout.vue";
+import {useAppStore} from "@/store/modules/app.js";
+
+const route = useRoute();
+const appStore = useAppStore();
+const theme = reactive({
+  token: {
+    colorPrimary: appStore.theme.colorPrimary,
+    borderRadius: 0,
+  },
+});
 </script>
 
 <template>
-
+  <a-config-provider :theme="theme">
+    <template v-if="route.meta.visible">
+      <Layout/>
+    </template>
+    <template v-else>
+      <router-view/>
+    </template>
+  </a-config-provider>
 </template>
-
-<style scoped>
-
-</style>
