78a5a85be3ad0638060e566797f24a10100a51cb
liuyoumi <liuyoumi@hotmail.com>
2025-04-01 14:51:47 +0800

    chore: simulate login test

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,24 @@
 <script setup>
+import {setToken} from "@/utils/auth.js";
 
+const router = useRouter();
+
+const onLogin = () => {
+  setToken("test");
+  router.push("/");
+};
 </script>
 
 <template>
-  <h1>Login</h1>
+  <div class="mx-auto mt-50 w-100">
+    <a-form>
+      <a-form-item>
+        <a-input placeholder="账号"></a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-input type="password" placeholder="密码"></a-input>
+      </a-form-item>
+      <a-button type="primary" @click="onLogin">登录</a-button>
+    </a-form>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
