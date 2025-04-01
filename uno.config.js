d07131fdb678aaea1dfb1c98c7facec273790897
liuyoumi <liuyoumi@hotmail.com>
2025-04-01 14:47:31 +0800

    chore: add ad dominant color

--- uno.config.js
+++ uno.config.js
@@ -2,6 +2,11 @@ import {defineConfig} from "unocss";
 import transformerVariantGroup from "@unocss/transformer-variant-group";
 
 export default defineConfig({
+  theme: {
+    colors: {
+      primary: "var(--color-primary)",
+    },
+  },
   transformers: [
     transformerVariantGroup(),
   ],
