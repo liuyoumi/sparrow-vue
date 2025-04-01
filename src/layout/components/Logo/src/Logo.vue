7ec29c26a9535ae507ca29ab87cdf3d79ec1bcc0
liuyoumi <liuyoumi@hotmail.com>
2025-04-01 14:49:52 +0800

    feat: add website logo

--- src/layout/components/Logo/src/Logo.vue
+++ src/layout/components/Logo/src/Logo.vue
@@ -3,9 +3,19 @@
 </script>
 
 <template>
-  $END$
+  <div class="logo">
+    HuiMi-Vue
+  </div>
 </template>
 
 <style scoped>
-
+.logo {
+  height: var(--logo-height);
+  color: var(--color-primary);
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+}
 </style>
