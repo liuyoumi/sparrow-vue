291079768f517a2f69ae6440776319c3eeeda1f1
liuyoumi <liuyoumi@hotmail.com>
2025-04-01 14:50:43 +0800

    feat: add size and color props

--- src/components/Icon/src/Icon.vue
+++ src/components/Icon/src/Icon.vue
@@ -1,15 +1,23 @@
 <script setup>
-import {IconPark} from "@icon-park/vue-next/es/all.js";
+import {IconPark} from "@icon-park/vue-next/lib/all.js";
 
 defineProps({
   name: {
     type: String,
     required: true,
   },
+  size: {
+    type: [Number, String],
+    default: "1.2em",
+  },
+  color: {
+    type: String,
+    default: "currentColor",
+  },
 });
 </script>
 
 <template>
   <!-- 添加的两个className是为了适配 antd 原生的图标样式 -->
-  <IconPark :type="name" class="anticon anticon-pie-chart" style="vertical-align: -2px"/>
+  <IconPark :type="name" :size="size" class="anticon anticon-pie-chart" :fill="color"/>
 </template>
